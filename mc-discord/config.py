d2d15bdfc1e31c221190283a23436d32388a27ef
Bahaidz <bahaidz102@gmail.com>
2026-05-10 21:22:41 +0700

    Create config.py

---
+++ mc-discord/config.py
@@ -0,0 +1,17 @@
+# ========== DISCORD BOT ==========
+TOKEN = "TOKEN DISCORD"
+PREFIX = "!"
+BOT_NAME = "NAME BOT"
+
+# ========== MINECRAFT SERVER ==========
+SERVER_IP = "IP SEVER MINECRAFT"
+SERVER_PORT = PORT SEVER MINECRAFT
+CHECK_INTERVAL = 30  
+
+# ========== KÊNH AUTO THÔNG BÁO ==========
+CHANNEL_ID = ID CHANNEL DISCORD
+
+# ========== PICK ROLE ==========
+ROLE_ID = ID ROLE DISCORD          # ID role xanh
+ROLE_EMOJI = "NAME EMOJI"             # Emoji nút
+ROLE_LABEL = "COLOR"           # Nhãn nút
